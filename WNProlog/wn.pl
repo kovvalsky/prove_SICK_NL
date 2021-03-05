2a1de8326c830ae8a40dc54cf9bb1cdf5c078126
kovvalsky <lasha.abzianidze@gmail.com>
2021-03-05 17:05:31 +0100

    Add OpenDutchWordNet patch for man.n.03 and boom being tree

--- WNProlog/wn.pl
+++ WNProlog/wn.pl
@@ -7,3 +7,14 @@
 	%'wn_der',
 	'wn_s'
 	]).
+
+:- dynamic s/6.
+:- dynamic hyp/2.
+
+
+odwn_patch :-
+	retractall(s('eng-30-00007846-n', _, 'man', 'n', 3, _)),
+	assertz(hyp('eng-30-13104059-n', 'eng-30-12212361-n')).
+
+% block certain unwanted senses
+:- odwn_patch.
