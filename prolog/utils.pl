384995486b6540e878d1f5383e054b9b11473cb1
kovvalsky <lasha.abzianidze@gmail.com>
2021-03-05 17:01:54 +0100

    Add lemma rewriting rules for (n)iemand, (n)iets, is, zijn, worden, aan_het

--- prolog/utils.pl
+++ prolog/utils.pl
@@ -55,9 +55,19 @@ translate_nl2en((tlp(T,NL,P), Ty), (tlp(T,EN,P), Ty)) :- !,
     ; memberchk(NL, ['het','de']) -> EN = 'the'
     ; memberchk(NL, ['een','één']) -> EN = 'a'
     ; NL == 'en' -> EN = 'and'
+    ; memberchk(NL, ['is','zijn']), Ty = np:_~>s:_ -> EN = 'be' % pos=RB?
     ; NL == 'er' -> EN = 'there'
     ; NL == 'die', Ty = (np:_~>s:_)~>N~>N -> EN = 'who'
     ; NL == 'door', Ty = np:_~>(np:_~>s:_)~>(np:_~>s:_) -> EN = 'by'
+    ; NL == 'iemand', Ty = np:_ -> EN = 'somebody'
+    ; NL == 'niemand', Ty = np:_ -> EN = 'nobody'
+    ; NL == 'iets', Ty = np:_ -> EN = 'something'
+    ; NL == 'niets', Ty = np:_ -> EN = 'nothing'
+    ; NL == 'worden', P == 'RB', Ty = (np:_~>s:pt)~>np:_~>s:_ -> EN = 'be'
+    % ; NL == 'doen', Ty = np:_~>np:_~>s:_ -> EN = 'do'%!!! plaatsen<doen sicknl-3250
+    ; memberchk(NL, ['is','aan_het']),
+      memberchk(P, ['RB','AUX']),
+      Ty = (np:_~>s:_)~>np:_~>s:_ -> EN = 'be'
     ; NL = EN ).
 
 
