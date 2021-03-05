f8e28a714b385cd9e8f461fff167284f30353f81
kovvalsky <lasha.abzianidze@gmail.com>
2021-03-05 17:02:39 +0100

    dump json with unicode

--- python/utils.py
+++ python/utils.py
@@ -15,7 +15,7 @@ def write_json(filename, sen_annotations):
     with open(filename, 'w') as F:
         anno_list = []
         for i, toks in sen_annotations.items():
-            tokens = ',\n    '.join([ json.dumps(t) for t in toks ])
+            tokens = ',\n    '.join([ json.dumps(t, ensure_ascii=False) for t in toks ])
             anno_list.append(f'  "{i}": [\n    {tokens} ]')
         content = ',\n'.join(anno_list)
         F.write(f'{{\n{content}\n}}')
