6fb15310ee9bac8ebef7c074cae78cdb6aeec6d4
kowalsky <lasha.abzianidze@gmail.com>
2021-03-25 18:13:20 +0100

    Add results dir

---
+++ Results/TE/r50.ans_KB.pl
@@ -0,0 +1,376 @@
+%%%%%%%%%%%%% Parameters of parList %%%%%%%%%%%%%%
+%%% [nil,effCr([equi,nonBr,nonProd,nonCons]),lang(nl),anno_json(SICK_NL/anno/spacy_lg.json),waif(Results/TE/r50.ans),complete_tree,allInt,aall,wn_ant,wn_sim,wn_der,constchck,parallel(8),ral(50),parts([TRIAL,TRAIN])]
+
+%%%%%%%%%%%%%%%% Induction config %%%%%%%%%%%%%%%%
+%%% [align-both,constchk,constKB,compTerms,patterns-[_12,_14@_16,_18@_20@_22,_28@(_24@_26)]]
+%%%%%%%%%%%%%%% Induced Knowledge %%%%%%%%%%%%%%%%
+ind_rel(isa_wn(zwembad,'is in@(the@(van@(the@oceaan)@buurt))')).
+ind_rel(isa_wn(zwart,is)).
+ind_rel(isa_wn(zwart,donker)).
+ind_rel(isa_wn(zwaard,speelgoedwapen)).
+ind_rel(isa_wn(zwaard,mes)).
+ind_rel(isa_wn(zonnebril,'donker bril')).
+ind_rel(isa_wn('zitten@(onder@(a@(paars@paraplu)))@(the@man)','buitenshuis zitten@(onder@(a@(paars@paraplu)))@(the@man)')).
+ind_rel(isa_wn('zitten achterna',achtervolgen)).
+ind_rel(isa_wn(zingen,'zingen a@gitaar')).
+ind_rel(isa_wn(zien,snijden)).
+ind_rel(isa_wn(zetten,stoppen)).
+ind_rel(isa_wn(zetten,inpakken)).
+ind_rel(isa_wn(zetten,'brengen aan')).
+ind_rel(isa_wn(zeggen,praten)).
+ind_rel(isa_wn(zeekust,strand)).
+ind_rel(isa_wn(zandstrand,zandkust)).
+ind_rel(isa_wn('worden knippen',knippen)).
+ind_rel(isa_wn(wissen,weten)).
+ind_rel(isa_wn(winkel,gebouw)).
+ind_rel(isa_wn(water,waterlichaam)).
+ind_rel(isa_wn(wakeboarden,waterskiën)).
+ind_rel(isa_wn(vuren,schieten)).
+ind_rel(isa_wn('vuil water','modderig water')).
+ind_rel(isa_wn(vrouw,oudje)).
+ind_rel(isa_wn(vrouw,'op@(the@strand) be')).
+ind_rel(isa_wn(vrouw,meisje)).
+ind_rel(isa_wn(vrouw,kok)).
+ind_rel(isa_wn(vrouw,'elegant vrouw')).
+ind_rel(isa_wn(voor,om)).
+ind_rel(isa_wn(voor,naar)).
+ind_rel(isa_wn(voor,bij)).
+ind_rel(isa_wn(volleybal,bal)).
+ind_rel(isa_wn(voetballer,voetbalspeler)).
+ind_rel(isa_wn(voetbal,voetbalwedstrijd)).
+ind_rel(isa_wn(voetbal,bal)).
+ind_rel(isa_wn(voertuig,auto)).
+ind_rel(isa_wn(vlug,snel)).
+ind_rel(isa_wn(vloeroefening,oefening)).
+ind_rel(isa_wn(vloer,grond)).
+ind_rel(isa_wn(vasthouden,'houden vast')).
+ind_rel(isa_wn(varen,razen)).
+ind_rel(isa_wn(van,'van a@rots')).
+ind_rel(isa_wn('vallen@(in@(the@gras))',voetballen)).
+ind_rel(isa_wn(uitvoeren,'varen uit')).
+ind_rel(isa_wn('twee@mens','a@motorfiets')).
+ind_rel(isa_wn(tuin,achtertuin)).
+ind_rel(isa_wn(trui,'zwart trui')).
+ind_rel(isa_wn(trouwjurk,bruidssluier)).
+ind_rel(isa_wn('trekken op',stoppen)).
+ind_rel(isa_wn('treden op',spelen)).
+ind_rel(isa_wn(trede,trap)).
+ind_rel(isa_wn(touw,speeltje)).
+ind_rel(isa_wn(tomaat,groente)).
+ind_rel(isa_wn('tikken af',vallen)).
+ind_rel(isa_wn('tikken aan',raken)).
+ind_rel(isa_wn('the meisje','eèn meisje')).
+ind_rel(isa_wn('the meer','the water')).
+ind_rel(isa_wn('the figuur','enkel figuur')).
+ind_rel(isa_wn(the,twee)).
+ind_rel(isa_wn(tennisbal,bal)).
+ind_rel(isa_wn(tekenfilmvliegtuig,vliegtuig)).
+ind_rel(isa_wn(tekenfilmvliegtuig,'tekenen vliegtuig')).
+ind_rel(isa_wn(teckel,hond)).
+ind_rel(isa_wn(tatoeage,'op@(the@(van@(a@tiener)@hand)) tatoeage')).
+ind_rel(isa_wn(suiker,kruid)).
+ind_rel(isa_wn(stuk,'worden knippen')).
+ind_rel(isa_wn(stuk,'speelgoed stuk')).
+ind_rel(isa_wn(stuk,'hout stuk')).
+ind_rel(isa_wn('strooien uit',verspreiden)).
+ind_rel(isa_wn(strooien,leggen)).
+ind_rel(isa_wn(strand,zand)).
+ind_rel(isa_wn(strand,wal)).
+ind_rel(isa_wn(straat,stadsstraat)).
+ind_rel(isa_wn(ster,lucht)).
+ind_rel(isa_wn(steken,lopen)).
+ind_rel(isa_wn(standbeeld,'gouden standbeeld')).
+ind_rel(isa_wn(staan,waden)).
+ind_rel(isa_wn(staan,wachten)).
+ind_rel(isa_wn(staan,'staan op')).
+ind_rel(isa_wn(staan,'staan achter')).
+ind_rel(isa_wn(sprint,'door@(the@gras) rennen')).
+ind_rel(isa_wn('springen op',opstaan)).
+ind_rel(isa_wn('springen af',springen)).
+ind_rel(isa_wn(springen,stuiteren)).
+ind_rel(isa_wn(springen,'springen af')).
+ind_rel(isa_wn(springen,klimmen)).
+ind_rel(isa_wn(springen,duiken)).
+ind_rel(isa_wn(springen,'bespringen a@muur')).
+ind_rel(isa_wn('spoelen af',spoelen)).
+ind_rel(isa_wn(spoelen,'spoelen af')).
+ind_rel(isa_wn(spies,spiesje)).
+ind_rel(isa_wn(spetteren,spatten)).
+ind_rel(isa_wn('spelen a@gitaar',zingen)).
+ind_rel(isa_wn(spelen,tokkelen)).
+ind_rel(isa_wn(spelen,'spelen the@piano')).
+ind_rel(isa_wn(spelen,'spelen buiten')).
+ind_rel(isa_wn(spelen,'op@(the@trommel) oefenen')).
+ind_rel(isa_wn(spelen,glijden)).
+ind_rel(isa_wn(speelgoedtelefoon,'speelgoed stuk')).
+ind_rel(isa_wn(speelgoedkonijn,knuffelkonijn)).
+ind_rel(isa_wn(spatten,spetteren)).
+ind_rel(isa_wn(spannen,draaien)).
+ind_rel(isa_wn('snijden door',snijden)).
+ind_rel(isa_wn(snijden,hakken)).
+ind_rel(isa_wn(snijden,doorsnijden)).
+ind_rel(isa_wn(slaan,'tikken af')).
+ind_rel(isa_wn(slaan,'sterk kloppen')).
+ind_rel(isa_wn(sketch,'erg grappig')).
+ind_rel(isa_wn(shirtloz,hemdloz)).
+ind_rel(isa_wn(schroeven,'vastschroeven aan@(a@muur)')).
+ind_rel(isa_wn(schoenloz,schoenloos)).
+ind_rel(isa_wn(rust,'op@(the@boomstam) liggen')).
+ind_rel(isa_wn(roodgekleurd,rood)).
+ind_rel(isa_wn(rond,in)).
+ind_rel(isa_wn(roeren,kloppen)).
+ind_rel(isa_wn(rivier,waterlichaam)).
+ind_rel(isa_wn(rijden,bewegen)).
+ind_rel(isa_wn(rijden,'besturen twee@mens')).
+ind_rel(isa_wn(rijden,besturen)).
+ind_rel(isa_wn(richten,trekken)).
+ind_rel(isa_wn(rennen,'sprint in@(the@water)')).
+ind_rel(isa_wn(rennen,lopen)).
+ind_rel(isa_wn(rennen,'afrennen the@weg')).
+ind_rel(isa_wn(regenpla,'regen plas')).
+ind_rel(isa_wn(raken,'tikken aan')).
+ind_rel(isa_wn(raceauto,racevoertuig)).
+ind_rel(isa_wn(praten,telefoneren)).
+ind_rel(isa_wn(praten,bellen)).
+ind_rel(isa_wn(politiemotor,politiefiet)).
+ind_rel(isa_wn(poetsen,schoonmaken)).
+ind_rel(isa_wn(podium,piano)).
+ind_rel(isa_wn(pizza,voedsel)).
+ind_rel(isa_wn(pet,hoed)).
+ind_rel(isa_wn(persoon,groep)).
+ind_rel(isa_wn(persoon,fietser)).
+ind_rel(isa_wn(persen,knijpen)).
+ind_rel(isa_wn(pauzeren,stoppen)).
+ind_rel(isa_wn(paneren,omhullen)).
+ind_rel(isa_wn(pandabeer,'zijn beer')).
+ind_rel(isa_wn(pandabeer,'schattig panda')).
+ind_rel(isa_wn(pan,koekenpan)).
+ind_rel(isa_wn('pakken the@bal',speler)).
+ind_rel(isa_wn('paars@pak','kleuren paars@pak')).
+ind_rel(isa_wn('paars@hoed','met@(bloot@hand) paars@hoed')).
+ind_rel(isa_wn(paard,dier)).
+ind_rel(isa_wn(over,door)).
+ind_rel(isa_wn(opblaasbaar,groen)).
+ind_rel(isa_wn('op the@gras','in the@gras')).
+ind_rel(isa_wn('op a@bed','in a@bed')).
+ind_rel(isa_wn(op,naar)).
+ind_rel(isa_wn(op,met)).
+ind_rel(isa_wn(op,in)).
+ind_rel(isa_wn(op,door)).
+ind_rel(isa_wn(op,aan)).
+ind_rel(isa_wn(ontwaken,'worden wakker')).
+ind_rel(isa_wn(onbevreesd,is)).
+ind_rel(isa_wn(om,voor)).
+ind_rel(isa_wn(olijfolie,bakolie)).
+ind_rel(isa_wn(oever,zand)).
+ind_rel(isa_wn(oefenen,sporten)).
+ind_rel(isa_wn(naar,met)).
+ind_rel(isa_wn(muur,'baksteen muur')).
+ind_rel(isa_wn(muis,dier)).
+ind_rel(isa_wn(motorfiets,politiemotor)).
+ind_rel(isa_wn(motor,motorcrossmotor)).
+ind_rel(isa_wn(motor,fiets)).
+ind_rel(isa_wn(monument,'roodsten structuur')).
+ind_rel(isa_wn(met,tegen)).
+ind_rel(isa_wn(met,op)).
+ind_rel(isa_wn(met,naar)).
+ind_rel(isa_wn(met,af)).
+ind_rel(isa_wn(mes,wapen)).
+ind_rel(isa_wn(mens,voetballer)).
+ind_rel(isa_wn(menigte,mens)).
+ind_rel(isa_wn(meisje,'jong dame')).
+ind_rel(isa_wn(markt,winkel)).
+ind_rel(isa_wn(man,wandelaar)).
+ind_rel(isa_wn(man,speler)).
+ind_rel(isa_wn(man,racer)).
+ind_rel(isa_wn(man,muzikant)).
+ind_rel(isa_wn(man,motorrijder)).
+ind_rel(isa_wn(man,model)).
+ind_rel(isa_wn(man,'dragen the@strohoed')).
+ind_rel(isa_wn(man,'dragen a@helm')).
+ind_rel(isa_wn(man,'aannemen a@pose')).
+ind_rel(isa_wn(maki,dier)).
+ind_rel(isa_wn('maken vast','brengen aan')).
+ind_rel(isa_wn('maken schoon',uitwissen)).
+ind_rel(isa_wn('maken schoon',reinigen)).
+ind_rel(isa_wn('maken bang','doen schrikken')).
+ind_rel(isa_wn(maken,tekenen)).
+ind_rel(isa_wn(maken,nemen)).
+ind_rel(isa_wn(machine,naaimachine)).
+ind_rel(isa_wn(lucht,'alleen lucht')).
+ind_rel(isa_wn('lopen rond',lopen)).
+ind_rel(isa_wn(lopen,rennen)).
+ind_rel(isa_wn(lopen,'lopen langzaam')).
+ind_rel(isa_wn(lopen,'lopen buiten')).
+ind_rel(isa_wn(lens,cameralen)).
+ind_rel(isa_wn(leggen,strooien)).
+ind_rel(isa_wn(leggen,'laten vallen')).
+ind_rel(isa_wn(langs,over)).
+ind_rel(isa_wn(landschap,sneeuwlandschap)).
+ind_rel(isa_wn(lachen,glimlachen)).
+ind_rel(isa_wn(lachen,be)).
+ind_rel(isa_wn(kraanvogel,kraan)).
+ind_rel(isa_wn(kostuum,jurk)).
+ind_rel(isa_wn(komen,'komen tevoorschijn')).
+ind_rel(isa_wn(koken,roosteren)).
+ind_rel(isa_wn(knuffelkonijn,knuffelhaasje)).
+ind_rel(isa_wn('klimmen af','beklimmen a@rotswand')).
+ind_rel(isa_wn('klein kat',kit)).
+ind_rel(isa_wn(klein,jong)).
+ind_rel(isa_wn(kind,jongen)).
+ind_rel(isa_wn(kind,'jong kind')).
+ind_rel(isa_wn(kind,'blond kind')).
+ind_rel(isa_wn('kijken@(in@(dezelfde@(binnenin@(a@bamboestructuur)@richting)))@(a@(vol@(met@(a@leerling))@klaslokaal))','aandachtig kijken@(in@(dezelfde@(binnenin@(a@bamboestructuur)@richting)))@(a@(vol@(met@(a@leerling))@klaslokaal))')).
+ind_rel(isa_wn('kijken toe',kijken)).
+ind_rel(isa_wn(kijken,zijn)).
+ind_rel(isa_wn(kijken,'staart naar@(a@kalender)')).
+ind_rel(isa_wn(kijken,rondkijken)).
+ind_rel(isa_wn(kijken,controleren)).
+ind_rel(isa_wn(kijken,bestuderen)).
+ind_rel(isa_wn(kano,boot)).
+ind_rel(isa_wn(kamer,'slecht verlichten')).
+ind_rel(isa_wn(jongen,'met@(a@gras) bedekken')).
+ind_rel(isa_wn(jongen,jongeman)).
+ind_rel(isa_wn('jong twee@meisje','twee@meisje')).
+ind_rel(isa_wn('jong jong@(twee@kind)','jong@(twee@kind)')).
+ind_rel(isa_wn(jong,klein)).
+ind_rel(isa_wn(jeep,voertuig)).
+ind_rel(isa_wn(jagen,vangen)).
+ind_rel(isa_wn(internetcamera,webcam)).
+ind_rel(isa_wn(inpakken,stoppen)).
+ind_rel(isa_wn('in@(a@schijf) snijden',tofu)).
+ind_rel(isa_wn('in the@sneeuw','door the@sneeuw')).
+ind_rel(isa_wn(in,rond)).
+ind_rel(isa_wn(in,op)).
+ind_rel(isa_wn(in,door)).
+ind_rel(isa_wn(in,aan)).
+ind_rel(isa_wn(huis,'stenen huis')).
+ind_rel(isa_wn('houden vast',vasthouden)).
+ind_rel(isa_wn(houden,'houden vast')).
+ind_rel(isa_wn(honkloper,speler)).
+ind_rel(isa_wn(hond,'in@(the@gras) hond')).
+ind_rel(isa_wn(hoed,cowboyhoed)).
+ind_rel(isa_wn(heuvel,sneeuw)).
+ind_rel(isa_wn(helling,schans)).
+ind_rel(isa_wn(haveloos,breken)).
+ind_rel(isa_wn('hangen rond',lopen)).
+ind_rel(isa_wn(halter,gewicht)).
+ind_rel(isa_wn(groot,fors)).
+ind_rel(isa_wn(groot,enorm)).
+ind_rel(isa_wn(groep,roedel)).
+ind_rel(isa_wn(groep,menigte)).
+ind_rel(isa_wn(groen,'kleuren groen')).
+ind_rel(isa_wn('grijs@\'t-shirt\'','verven grijs@\'t-shirt\'')).
+ind_rel(isa_wn(grazen,voeden)).
+ind_rel(isa_wn(gras,gazon)).
+ind_rel(isa_wn(goochelaar,man)).
+ind_rel(isa_wn(golf,oceaangolv)).
+ind_rel(isa_wn(glijden,kruipen)).
+ind_rel(isa_wn(genezen,'doen herleven')).
+ind_rel(isa_wn(gebied,'besneeuwd gebied')).
+ind_rel(isa_wn(gazon,gras)).
+ind_rel(isa_wn(fontein,grondfontein)).
+ind_rel(isa_wn(eten,verslinden)).
+ind_rel(isa_wn(eten,bijten)).
+ind_rel(isa_wn('enkel rots','a rots')).
+ind_rel(isa_wn('enkel boom','a boom')).
+ind_rel(isa_wn(egel,'klein dier')).
+ind_rel(isa_wn('duiken af',duiken)).
+ind_rel(isa_wn(drumstel,trommel)).
+ind_rel(isa_wn(drie,enkel)).
+ind_rel(isa_wn(draven,rijden)).
+ind_rel(isa_wn(draaitafel,mengpaneel)).
+ind_rel(isa_wn(draaien,lopen)).
+ind_rel(isa_wn(door,over)).
+ind_rel(isa_wn(door,op)).
+ind_rel(isa_wn(door,om)).
+ind_rel(isa_wn(door,in)).
+ind_rel(isa_wn(doen,stoppen)).
+ind_rel(isa_wn(dichtbij,dichtbijgelegen)).
+ind_rel(isa_wn(crosser,fietser)).
+ind_rel(isa_wn('cornflakes komen',ontbijtgraan)).
+ind_rel(isa_wn(cornflakes,ontbijtgraan)).
+ind_rel(isa_wn(container,'plastic container')).
+ind_rel(isa_wn(clown,'gekostumeerd artiest')).
+ind_rel(isa_wn(circuit,parcours)).
+ind_rel(isa_wn(chef,'chef-kok')).
+ind_rel(isa_wn(camera,foto)).
+ind_rel(isa_wn(bungelen,schoppen)).
+ind_rel(isa_wn(buiten,rond)).
+ind_rel(isa_wn(bruingeel,bruin)).
+ind_rel(isa_wn('brengen aan',opdoen)).
+ind_rel(isa_wn(bouw,werk)).
+ind_rel(isa_wn(boren,maken)).
+ind_rel(isa_wn(boomtak,boomstam)).
+ind_rel(isa_wn(boom,'bladloos boom')).
+ind_rel(isa_wn(boa,sjaal)).
+ind_rel(isa_wn(blok,schijf)).
+ind_rel(isa_wn('blauw@jas','kleuren blauw@jas')).
+ind_rel(isa_wn('blauw@gympak','kleuren blauw@gympak')).
+ind_rel(isa_wn(blaffen,zijn)).
+ind_rel(isa_wn(blaffen,praten)).
+ind_rel(isa_wn('bij wit@(drie@hond)','dicht_bij wit@(drie@hond)')).
+ind_rel(isa_wn(bier,drank)).
+ind_rel(isa_wn(bespuiten,'vol a@graffiti')).
+ind_rel(isa_wn('beschermen kleding',uitrusting)).
+ind_rel(isa_wn(bekijken,bijwonen)).
+ind_rel(isa_wn(bamboestok,bamboe)).
+ind_rel(isa_wn(bal,'speelgoed stuk')).
+ind_rel(isa_wn(badpak,zwemkleding)).
+ind_rel(isa_wn(auto,voertuig)).
+ind_rel(isa_wn(aubergine,groente)).
+ind_rel(isa_wn(af,met)).
+ind_rel(isa_wn(acteren,'treden op')).
+ind_rel(isa_wn(accessoirezaak,accessoirewinkel)).
+ind_rel(isa_wn(aarde,gat)).
+ind_rel(isa_wn(aardappel,voedsel)).
+ind_rel(isa_wn(aap,'in@(the@dierentuin) aap')).
+ind_rel(isa_wn(aap,chimpansee)).
+ind_rel(isa_wn(aan,op)).
+ind_rel(isa_wn(aan,in)).
+ind_rel(isa_wn(aan,achter)).
+ind_rel(isa_wn('a@motorfiets','twee@mens')).
+ind_rel(isa_wn('a tafeltennis','a pingpongen')).
+ind_rel(isa_wn('a piano','a keyboard')).
+ind_rel(isa_wn('a komen','a schaal')).
+ind_rel(isa_wn('a komen','a kom')).
+ind_rel(isa_wn('a gras','no hooi')).
+ind_rel(isa_wn('a fluiten','a fluit')).
+ind_rel(isa_wn('a fluiten','a blaasinstrument')).
+ind_rel(disj('zitten aan','zorgen@(voor@(a@camouflage))')).
+ind_rel(disj('zetten aan','zetten uit')).
+ind_rel(disj(waden,zitten)).
+ind_rel(disj(visgerei,visstok)).
+ind_rel(disj(rijden,stappen)).
+ind_rel(disj('rennen in@(the@veld)',staan)).
+ind_rel(disj(rennen,stoppen)).
+ind_rel(disj(onthullen,verbergen)).
+ind_rel(disj(missen,slaan)).
+ind_rel(disj(missen,raken)).
+ind_rel(disj(maracas,tamboerijn)).
+ind_rel(disj('lopen weg',zetten)).
+ind_rel(disj(lopen,'rennen binnenshuis')).
+ind_rel(disj('liggen in@(the@gras)',rennen)).
+ind_rel(disj(liggen,'lopen rond')).
+ind_rel(disj(leeg,vol)).
+ind_rel(disj(lachen,vast)).
+ind_rel(disj(kind,stel)).
+ind_rel(disj(is,ver)).
+ind_rel(disj(in,'met a@machine')).
+ind_rel(disj(gieten,halen)).
+ind_rel(disj(geven,'wijzen af')).
+ind_rel(disj('drinken a@sinaasappelsap',lopen)).
+ind_rel(disj('dragen spul',naakt)).
+ind_rel(disj(dier,vrouw)).
+ind_rel(disj(dicht,'in@(the@gras) lopen')).
+ind_rel(disj('de_nacht fietsen@(in@(a@(besneeuwd@bos)))@(a@fietser)','overdag fietsen@(in@(a@(besneeuwd@bos)))@(a@fietser)')).
+ind_rel(disj(boren,sluiten)).
+ind_rel(disj('bij a@park','met jong@(twee@jongen)')).
+ind_rel(disj(bevuilen,schoonmaken)).
+ind_rel(disj(bespelen,stoppen)).
+ind_rel(disj(bespelen,opbergen)).
+ind_rel(disj(be,'staan@(op@(a@(rotsachtig@ondergrond)))')).
+ind_rel(disj(be,'spelen@(in@(the@zand))')).
+ind_rel(disj('a hond','wit@(twee@hond)')).
+ind_rel(ant_wn(jong,'met@(a@helm)')).
+ind_rel(ant_wn('and@(a@man)',jong)).
